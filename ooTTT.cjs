a2e6eac0f4ff8e496e3716e773c5fe85071db8da
Nolan Juusola <noljuusola@gmail.com>
2023-11-11 14:19:21 -0600

    changes to ooTTT

--- ooTTT.cjs
+++ ooTTT.cjs
@@ -1,23 +1,58 @@
 let readline = require('readline-sync');
 
-class Player {
-  constructor(order) {
-    this.resetScore();
-    this.order = order;
-    this.playerMarker = this.order === Player.PLAYER_ONE_ORDER ?
-      Player.P1_MARKER : Player.P2_MARKER;
-  }
-  static PLAYER_ONE_ORDER = 1;
-  static PLAYER_TWO_ORDER = 2;
+class Statics {
+  static P1_ORDER = 1;
+  static P2_ORDER = 2;
   static PLAYER_IS_HUMAN = 'human';
   static PLAYER_IS_COMPUTER = 'computer';
   static COMPUTER_EASY = 'easy';
   static COMPUTER_HARD = 'hard';
   static P1_MARKER = 'X';
   static P2_MARKER = 'O';
+  static UNUSED_SQUARE = ' ';
+
+  static joinOr(array, separator, finalWord = 'or') {
+    if (array.length === 1) {
+      return array[0];
+    } else if (array.length === 2) {
+      return array[0] + ' ' + finalWord + ' ' + array[1];
+    }
+    let string = '';
+    array.forEach((ele, index) => {
+      if (index === array.length - 2) {
+        string += (ele + " " + finalWord + ' ');
+      } else if (index === array.length - 1) {
+        string += (ele);
+      } else {
+        string += (ele + separator + ' ');
+      }
+    });
+    return string;
+  }
+
+  static WINNING_COMBOS = [
+    [ "1", "2", "3" ],
+    [ "4", "5", "6" ],
+    [ "7", "8", "9" ],
+    [ "1", "4", "7" ],
+    [ "2", "5", "8" ],
+    [ "3", "6", "9" ],
+    [ "1", "5", "9" ],
+    [ "3", "5", "7" ],
+  ]
+}
+
+
+class Player {
+  constructor(order) {
+    this.resetScore();
+    this.order = order;
+    this.playerMarker = this.order === Statics.P1_ORDER ?
+      Statics.P1_MARKER : Statics.P2_MARKER;
+  }
 
   getPlayerDifficulty() {
-    if (this.type === Player.PLAYER_IS_COMPUTER) {
+    if (this.type === Statics.PLAYER_IS_COMPUTER) {
       let response;
       while (true) {
         console.log(`Is ${this.name} on easy mode or hard mode (e, h)?`);
@@ -25,24 +60,13 @@ class Player {
         if (response === 'e' || response === 'h') break;
         console.log("That's not a valid choice.");
       }
-      this.difficulty = response === 'e' ? Player.COMPUTER_EASY : Player.COMPUTER_HARD;
+      this.difficulty = response === 'e' ? Statics.COMPUTER_EASY : Statics.COMPUTER_HARD;
     }
   }
 
   getName(order) {
     console.log(`Enter Player ${order}'s name:`);
     this.name = readline.question();
-  }
-
-  getType(order) {
-    let type;
-    while (true) {
-      console.log(`Is Player ${order} a human or a computer (h, c)?`);
-      type = (readline.question()).toLowerCase();
-      if (type === 'h' || type === 'c') break;
-      console.log("That's not a valid choice.");
-    }
-    this.type = type === 'h' ? Player.PLAYER_IS_HUMAN : Player.PLAYER_IS_COMPUTER;
   }
 
   returnName() {
@@ -58,8 +82,8 @@ class Player {
   }
 
   returnOpponentMarker() {
-    return this.returnMarker() === Player.P1_MARKER ?
-      Player.P2_MARKER : Player.P1_MARKER;
+    return this.returnMarker() === Statics.P1_MARKER ?
+      Statics.P2_MARKER : Statics.P1_MARKER;
   }
 
   resetScore() {
@@ -70,17 +94,96 @@ class Player {
     this.score++;
   }
 
+  makeMove(board) {
+    console.log(`${this.returnName()}'s turn:`);
+    return this.type === Statics.PLAYER_IS_HUMAN ?
+      board.markSquare(this.humanChoose(board), this.order) :
+      board.markSquare(this.computerMove(board), this.order);
+  }
+
+}
+
+class Human extends Player {
+  constructor(order) {
+    super(order);
+  }
+
+  humanChoose(board) {
+    let choice;
+    let choices = board.unusedSquares();
+
+    while (true) {
+      console.log(`Make your selection: (choices are: ${Statics.joinOr(choices, ',')})`);
+      choice = readline.question();
+      if (choices.includes(choice)) break;
+      console.log("That's not a valid choice.");
+    }
+    return choice;
+  }
+}
+
+class Computer extends Player {
+  constructor(order) {
+    super(order);
+  }
+
+  chooseRandom(board) {
+    let choice;
+    let choices = board.unusedSquares();
+    while (true) {
+      choice = String(Math.floor(Math.random() * 10));
+      if (choices.includes(choice)) break;
+    }
+    return choice;
+  }
+
+  returnCriticalSquare(marker, board) {
+    for (let idx = 0; idx < Statics.WINNING_COMBOS.length; idx++) {
+      let row = Statics.WINNING_COMBOS[idx];
+      let square = this.determineCriticalSquare(row, marker, board);
+      if (square) return square;
+    }
+    return null;
+  }
+
+  determineCriticalSquare(row, marker, board) {
+    if (board.countMarkersInRow(row, marker) === 2) {
+      let square = row.find(square => {
+        return board.gameBoard[square].isUnused();
+      });
+      if (square) return square;
+    }
+    return null;
+  }
+
+  computerMove(board) {
+    console.log('Making choice...(press return to coninue)');
+    readline.question();
+    return this.difficulty === Statics.COMPUTER_EASY ?
+      this.chooseRandom(board) : this.smartComputerChoose(board);
+  }
+
+  smartComputerChoose(board) {
+    return this.computerOffense(board) || this.computerDefense(board) ||
+    board.isCenterOpen() || this.chooseRandom(board);
+  }
+
+  computerOffense(board) {
+    return this.returnCriticalSquare(this.returnMarker(), board);
+  }
+
+  computerDefense(board) {
+    return this.returnCriticalSquare(this.returnOpponentMarker(), board);
+  }
 }
 
 class Square {
-  constructor(marker = Square.UNUSED_SQUARE) {
+  constructor(marker = Statics.UNUSED_SQUARE) {
     this.marker = marker;
   }
 
-  static UNUSED_SQUARE = ' ';
-
   isUnused() {
-    return this.marker === Square.UNUSED_SQUARE;
+    return this.marker === Statics.UNUSED_SQUARE;
   }
 
   getMarker() {
@@ -104,7 +207,7 @@ class Board {
   displayBoard() {
     console.log("");
     console.log("     |     |");
-    console.log(`  ${this.gameBoard[1].getMarker()}  |  ${this.gameBoard["2"].getMarker()}  |  ${this.gameBoard["3"].getMarker()}`);
+    console.log(`  ${this.gameBoard['1'].marker}  |  ${this.gameBoard["2"].getMarker()}  |  ${this.gameBoard["3"].getMarker()}`);
     console.log("     |     |");
     console.log("-----+-----+-----");
     console.log("     |     |");
@@ -134,6 +237,10 @@ class Board {
     return unusedArray;
   }
 
+  isCenterOpen() {
+    return this.gameBoard['5'].isUnused() ? '5' : false;
+  }
+
   countMarkersInRow(row, marker) {
     let markers = row.filter(square => {
       return this.gameBoard[square].markersMatch(marker);
@@ -150,23 +257,12 @@ class Board {
   }
 
   markSquare(square, order) {
-    if (order === Player.PLAYER_ONE_ORDER) {
-      this.gameBoard[square].setMarker(Player.P1_MARKER);
+    if (order === Statics.P1_ORDER) {
+      this.gameBoard[square].setMarker(Statics.P1_MARKER);
     } else {
-      this.gameBoard[square].setMarker(Player.P2_MARKER);
+      this.gameBoard[square].setMarker(Statics.P2_MARKER);
     }
   }
-
-  static WINNING_COMBOS = [
-    [ "1", "2", "3" ],
-    [ "4", "5", "6" ],
-    [ "7", "8", "9" ],
-    [ "1", "4", "7" ],
-    [ "2", "5", "8" ],
-    [ "3", "6", "9" ],
-    [ "1", "5", "9" ],
-    [ "3", "5", "7" ],
-  ]
 }
 
 class TTTGame {
@@ -205,105 +301,13 @@ class TTTGame {
     this.board.displayBoard();
   }
 
-  computerChooseRandom() {
-    let choice;
-    let choices = this.board.unusedSquares();
-    while (true) {
-      choice = String(Math.floor(Math.random() * 10));
-      if (choices.includes(choice)) break;
-    }
-    return choice;
-  }
-
-  returnCriticalSquare(marker) {
-    for (let idx = 0; idx < Board.WINNING_COMBOS.length; idx++) {
-      let row = Board.WINNING_COMBOS[idx];
-      let square = this.determineCriticalSquare(row, marker);
-      if (square) return square;
-    }
-    return null;
-  }
-
-  determineCriticalSquare(row, marker) {
-    if (this.board.countMarkersInRow(row, marker) === 2) {
-      let square = row.find(square => {
-        return this.board.gameBoard[square].isUnused();
-      });
-      if (square) return square;
-    }
-    return null;
-  }
-
-  computerOffense(player) {
-    return this.returnCriticalSquare(player.returnMarker());
-  }
-
-  computerDefense(player) {
-    return this.returnCriticalSquare(player.returnOpponentMarker());
-  }
-
-  isCenterOpen() {
-    return this.board.gameBoard['5'].marker === Square.UNUSED_SQUARE ? '5' : false;
-  }
-
-  computerMove(player) {
-    console.log('Making choice...(press return to coninue)');
-    readline.question();
-    return player.difficulty === Player.COMPUTER_EASY ?
-      this.computerChooseRandom() : this.smartComputerChoose(player);
-  }
-
-  smartComputerChoose(player) {
-    return this.computerOffense(player) || this.computerDefense(player) ||
-    this.isCenterOpen() || this.computerChooseRandom();
-  }
-
-  static joinOr(array, separator, finalWord = 'or') {
-    if (array.length === 1) {
-      return array[0];
-    } else if (array.length === 2) {
-      return array[0] + ' ' + finalWord + ' ' + array[1];
-    }
-    let string = '';
-    array.forEach((ele, index) => {
-      if (index === array.length - 2) {
-        string += (ele + " " + finalWord + ' ');
-      } else if (index === array.length - 1) {
-        string += (ele);
-      } else {
-        string += (ele + separator + ' ');
-      }
-    });
-    return string;
-  }
-
-  humanChoose() {
-    let choice;
-    let choices = this.board.unusedSquares();
-
-    while (true) {
-      console.log(`Make your selection: (choices are: ${TTTGame.joinOr(choices, ',')})`);
-      choice = readline.question();
-      if (choices.includes(choice)) break;
-      console.log("That's not a valid choice.");
-    }
-    return choice;
-  }
-
-  makeMove(player) {
-    console.log(`${player.returnName()}'s turn:`);
-    return player.type === Player.PLAYER_IS_HUMAN ?
-      this.board.markSquare(this.humanChoose(), player.order) :
-      this.board.markSquare(this.computerMove(player), player.order);
-  }
-
   determineGameWinner() {
-    for (let idx = 0; idx < Board.WINNING_COMBOS.length; idx++) {
+    for (let idx = 0; idx < Statics.WINNING_COMBOS.length; idx++) {
       // eslint-disable-next-line max-len
-      if (this.board.isWinningRow(Board.WINNING_COMBOS[idx], Player.P1_MARKER)) {
+      if (this.board.isWinningRow(Statics.WINNING_COMBOS[idx], Statics.P1_MARKER)) {
         return this.player1.returnName();
       // eslint-disable-next-line max-len
-      } else if (this.board.isWinningRow(Board.WINNING_COMBOS[idx], Player.P2_MARKER)) {
+      } else if (this.board.isWinningRow(Statics.WINNING_COMBOS[idx], Statics.P2_MARKER)) {
         return this.player2.returnName();
       }
     }
@@ -383,10 +387,12 @@ class TTTGame {
   playGame() {
     while (true) {
       this.scoreBoardDisplay();
-      this.makeMove(this.goesFirst());
+      this.firstPlayer = this.goesFirst();
+      this.secondPlayer = this.goesSecond();
+      this.firstPlayer.makeMove(this.board);
       this.scoreBoardDisplay();
       if (this.isGameOver()) break;
-      this.makeMove(this.goesSecond());
+      this.secondPlayer.makeMove(this.board);
       this.scoreBoardDisplay();
       if (this.isGameOver()) break;
     }
@@ -407,17 +413,30 @@ class TTTGame {
 
   createPlayer(order) {
     console.log(`~~~~~~~~~CREATE PLAYER ${order}~~~~~~~~~`);
-    let newPlayer = new Player(order);
-    newPlayer.getType(order);
+    let type = this.getType(order);
+    let newPlayer = type === Statics.PLAYER_IS_HUMAN ?
+      new Human(order) : new Computer(order);
+    newPlayer.type = type;
     newPlayer.getName(order);
-    newPlayer.getPlayerDifficulty(newPlayer);
+    newPlayer.getPlayerDifficulty();
     return newPlayer;
+  }
+
+  getType(order) {
+    let type;
+    while (true) {
+      console.log(`Is Player ${order} a human or a computer (h, c)?`);
+      type = (readline.question()).toLowerCase();
+      if (type === 'h' || type === 'c') break;
+      console.log("That's not a valid choice.");
+    }
+    return type === 'h' ? Statics.PLAYER_IS_HUMAN : Statics.PLAYER_IS_COMPUTER;
   }
 
   play() {
     this.displayWelcomeMessage();
-    this.player1 = this.createPlayer(Player.PLAYER_ONE_ORDER);
-    this.player2 = this.createPlayer(Player.PLAYER_TWO_ORDER);
+    this.player1 = this.createPlayer(Statics.P1_ORDER);
+    this.player2 = this.createPlayer(Statics.P2_ORDER);
     this.resetGame();
     while (true) {
       this.playMatch();
@@ -432,3 +451,4 @@ class TTTGame {
 let game = new TTTGame();
 
 game.play();
+
